f0a0fde6310c3c01d0a42f4894b0b93f41a8a89a
Dewnan Chamithka <dewnanchamithka2003@gmail.com>
2024-08-18 14:03:44 +0530

    updated nav bar

--- src/components/Head.js
+++ src/components/Head.js
@@ -7,20 +7,13 @@ const Head = () =>
         <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
             <div class="container-fluid">
                 <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                    <a class="navbar-brand mt-2 mt-lg-0" href="#">
-                        <img src="https://mdbcdn.b-cdn.net/img/logo/mdb-transaprent-noshadows.webp"height="15"alt="MDB Logo"loading="lazy" />
-                    </a>
-                    
-
+                        <img src="https://mdbcdn.b-cdn.net/img/logo/mdb-transaprent-noshadows.webp"height="15"alt="Logo"loading="lazy" />
                     <ul class="navbar-nav me-auto mb-2 mb-lg-0">
                         <li class="nav-item">
                             <a class="nav-link" href="#">Dashboard</a>
                         </li>
                         <li class="nav-item">
-                            <a class="nav-link" href="#">Team</a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link" href="#">Projects</a>
+                            <a class="nav-link" href="#">About</a>
                         </li>
                     </ul>
                 </div>
