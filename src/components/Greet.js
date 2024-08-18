8ae597704a46e0b56043465e21f4c884a323248d
Dewnan Chamithka <dewnanchamithka2003@gmail.com>
2024-08-18 13:11:35 +0530

    First Commit

---
+++ src/components/Greet.js
@@ -0,0 +1,9 @@
+import React from "react";
+/*
+function Greet() {
+    return <h2>Redda</h2>;
+}*/
+
+const Greet = () => <h2>Redda</h2>
+
+export default Greet;
---
+++ src/components/Head.js
@@ -0,0 +1,57 @@
+import React from "react";
+
+
+const Head = () =>
+    <div className="head">
+        
+        <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+            <div class="container-fluid">
+                <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                    <a class="navbar-brand mt-2 mt-lg-0" href="#">
+                        <img src="https://mdbcdn.b-cdn.net/img/logo/mdb-transaprent-noshadows.webp"height="15"alt="MDB Logo"loading="lazy" />
+                    </a>
+                    
+
+                    <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                        <li class="nav-item">
+                            <a class="nav-link" href="#">Dashboard</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#">Team</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#">Projects</a>
+                        </li>
+                    </ul>
+                </div>
+
+                <div class="profile_icon">
+                    <a class="dropdown-toggle d-flex align-items-center hidden-arrow"
+                        href="#"
+                        id="navbarDropdownMenuAvatar">
+                        <img
+                            src="https://mdbcdn.b-cdn.net/img/new/avatars/2.webp"
+                            class="rounded-circle"
+                            height="25"
+                            alt="Black and White Portrait of a Man"
+                            loading="lazy" />
+                    </a>
+                    <ul class="dropdown-menu dropdown-menu-end" aria-labelledby="navbarDropdownMenuAvatar">
+                        <li>
+                            <a class="dropdown-item" href="#">My profile</a>
+                        </li>
+                        <li>
+                            <a class="dropdown-item" href="#">Settings</a>
+                        </li>
+                        <li>
+                            <a class="dropdown-item" href="#">Logout</a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </nav>
+
+        
+    </div>
+    
+export default Head;
