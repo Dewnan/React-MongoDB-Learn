571149dd6c5c9660dd84655a84c668cb796c8e4f
Dewnan Chamithka <dewnanchamithka2003@gmail.com>
2024-08-19 16:58:32 +0530

    dark

--- src/components/Inventory.js
+++ src/components/Inventory.js
@@ -11,15 +11,32 @@ const Inventory = () => {
     }, []);
 
     return (
-        <div className="inv">
-            <h4>Inventory Items</h4>
-            <ul>
-                {items.map(item => (
-                    <li key={item._id}>
-                       Item id: {item.item_id} Name: {item.item_name} Stock: {item.stock}
-                    </li>
-                ))}
-            </ul>
+        <div class="inv">
+            <h1>Inventory Items</h1>
+            <div class="container table-responsive h-100 mask d-flex align-items-center row justify-content-center col-12">
+                <table class="table table-dark table-bordered mb-0">
+                    <thead>
+                        <tr>
+                            <th scope="col">EMPLOYEES</th>
+                            <th scope="col">POSITION</th>
+                            <th scope="col">CONTACTS</th>
+                            <th scope="col">AGE</th>
+                            <th scope="col">ADDRESS</th>
+                            <th scope="col">SALARY</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr>
+                            <th scope="row">Tiger Nixon</th>
+                            <td>System Architect</td>
+                            <td>tnixon12@example.com</td>
+                            <td>61</td>
+                            <td>Edinburgh</td>
+                            <td>$320,800</td>
+                        </tr>
+                    </tbody>
+                </table>
+            </div>
         </div>
     );
 };
