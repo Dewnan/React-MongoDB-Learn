b9b8b6c81e09ac3efb9a54308a908cabf8196af5
Dewnan Chamithka <dewnanchamithka2003@gmail.com>
2024-08-21 22:14:10 +0530

    updated

--- src/components/apiConfig.js
+++ src/components/apiConfig.js
@@ -1,4 +1,4 @@
-const apiUrl = 'backend-for-react-app-production.up.railway.app/api';
+const apiUrl = 'https://backend-for-react-app-production.up.railway.app/api';
 
 export const itemsAPI = `${apiUrl}/items`;
 export const customersAPI = `${apiUrl}/customers`;
