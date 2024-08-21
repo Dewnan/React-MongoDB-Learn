ab1003631d24b1ebc14d81d4cb22ef84920ca4d6
Dewnan Chamithka <dewnanchamithka2003@gmail.com>
2024-08-21 22:07:40 +0530

    updated url

--- src/components/apiConfig.js
+++ src/components/apiConfig.js
@@ -1,4 +1,4 @@
-const apiUrl = 'http://localhost:5000/api';
+const apiUrl = 'backend-for-react-app-production.up.railway.app:5000/api';
 
 export const itemsAPI = `${apiUrl}/items`;
 export const customersAPI = `${apiUrl}/customers`;
